2c5f38363e4339c0f1a916ed8f645d18296c6b6b
Thaiz <thaimoney@mail.ru>
2023-03-19 21:08:38 +0300

    19.03.2023

--- hero.py
+++ hero.py
@@ -1,10 +1,13 @@
 import pygame
+import settings
 
 
 class Hero():
     """Класс игровых персонажей"""
 
     def __init__(self, game, skin):
+        self.settings = settings.Settings()
+
         self.x = 500
         self.y = 400
 
@@ -22,7 +25,20 @@ class Hero():
         self.screen.blit(self.skin, (self.x, self.y))
 
     def move(self):
+        """Жвижение игрока"""
+        self._check_edges()
         if self.move_x:
             self.x += 4 * self.direction_x
         if self.move_y:
             self.y += 4 * self.direction_y
+
+    def _check_edges(self):
+        """Ограничивает возможность игрока выйти за пределы экрана"""
+        if self.x <= 0 and self.direction_x == -1:
+            self.move_x = False
+        elif self.x >= self.settings.screen_width - self.skin.get_width() and self.direction_x == 1:
+            self.move_x = False
+        if self.y <= 0 and self.direction_y == -1:
+            self.move_y = False
+        elif self.y >= self.settings.screen_height - self.skin.get_height() and self.direction_y == 1:
+            self.move_y = False
